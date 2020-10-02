241b20652d9f38279bf46ebb25de7bafdcbb0152
Emran Khandaker Evan <evanemran007@gmail.com>
2020-10-02 12:39:58 +0600

    First build.
    tetris with colors and 7 shapes added.

---
+++ Flappy-Bird/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        gameOverCanvas.SetActive(false);
+    }
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+}
---
+++ Flappy-Bird/Assets/PipeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float Maxtime = 1;
+    public float timer = 0;
+    public GameObject pipe;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > Maxtime)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newpipe, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
